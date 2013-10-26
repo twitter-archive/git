729bbcea374f7171a4dafd995992d3c1aa525cda
Thomas Rast <trast@inf.ethz.ch>
2013-10-26 16:43:18 +0200

    merge-recursive: -Xindex-only to leave worktree unchanged
    Using the new no_worktree flag from the previous commit, we can teach
    merge-recursive to leave the worktree untouched.  Expose this with a
    new strategy option so that scripts can use it.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- merge-recursive.c
+++ merge-recursive.c
@@ -2096,6 +2096,8 @@ int parse_merge_opt(struct merge_options *o, const char *s)
 		if ((o->rename_score = parse_rename_score(&score)) == -1 || *score != 0)
 			return -1;
 	}
+	else if (!strcmp(s, "index-only"))
+		o->no_worktree = 1;
 	else
 		return -1;
 	return 0;
