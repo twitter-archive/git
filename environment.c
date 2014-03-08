04bb3f34e872a1fb668d13dfa36a66ab1c729f9f
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2014-03-08 09:48:03 +0700

    $GIT_COMMON_DIR: a new environment variable
    This variable is intended to support multiple working directories
    attached to a repository. Such a repository may have a main working
    directory, created by either "git init" or "git clone" and one or more
    linked working directories. These working directories and the main
    repository share the same repository directory.
    
    In linked working directories, $GIT_COMMON_DIR must be defined to point
    to the real repository directory and $GIT_DIR points to an unused
    subdirectory inside $GIT_COMMON_DIR. File locations inside the
    repository are reorganized from the linked worktree view point:
    
     - worktree-specific such as HEAD, logs/HEAD, index, other top-level
       refs and unrecognized files are from $GIT_DIR.
    
     - the rest like objects, refs, info, hooks, packed-refs, shallow...
       are from $GIT_COMMON_DIR
    
    Scripts are supposed to retrieve paths in $GIT_DIR with "git rev-parse
    --git-path", which will take care of "$GIT_DIR vs $GIT_COMMON_DIR"
    business.
    
    The redirection is done by git_path(), git_pathdup() and
    strbuf_git_path(). The selected list of paths goes to $GIT_COMMON_DIR,
    not the other way around in case a developer adds a new
    worktree-specific file and it's accidentally promoted to be shared
    across repositories (this includes unknown files added by third party
    commands)
    
    The list of known files that belong to $GIT_DIR are:
    
    ADD_EDIT.patch BISECT_ANCESTORS_OK BISECT_EXPECTED_REV BISECT_LOG
    BISECT_NAMES CHERRY_PICK_HEAD COMMIT_MSG FETCH_HEAD HEAD MERGE_HEAD
    MERGE_MODE MERGE_RR NOTES_EDITMSG NOTES_MERGE_WORKTREE ORIG_HEAD
    REVERT_HEAD SQUASH_MSG TAG_EDITMSG fast_import_crash_* logs/HEAD
    next-index-* rebase-apply rebase-merge rsync-refs-* sequencer/*
    shallow_*
    
    Path mapping is NOT done for git_path_submodule(). Multi-checkouts are
    not supported as submodules.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- environment.c
+++ environment.c
@@ -80,9 +80,9 @@ static char *work_tree;
 static const char *namespace;
 static size_t namespace_len;
 
-static const char *git_dir;
+static const char *git_dir, *git_common_dir;
 static char *git_object_dir, *git_index_file, *git_graft_file;
-int git_db_env, git_index_env, git_graft_env;
+int git_db_env, git_index_env, git_graft_env, git_common_dir_env;
 
 /*
  * Repository-local GIT_* environment variables; see cache.h for details.
@@ -134,10 +134,16 @@ static void setup_git_env(void)
 		git_dir = DEFAULT_GIT_DIR_ENVIRONMENT;
 	gitfile = read_gitfile(git_dir);
 	git_dir = xstrdup(gitfile ? gitfile : git_dir);
+	git_common_dir = getenv(GIT_COMMON_DIR_ENVIRONMENT);
+	if (git_common_dir) {
+		git_common_dir_env = 1;
+		git_common_dir = xstrdup(git_common_dir);
+	} else
+		git_common_dir = git_dir;
 	git_object_dir = getenv(DB_ENVIRONMENT);
 	if (!git_object_dir) {
-		git_object_dir = xmalloc(strlen(git_dir) + 9);
-		sprintf(git_object_dir, "%s/objects", git_dir);
+		git_object_dir = xmalloc(strlen(git_common_dir) + 9);
+		sprintf(git_object_dir, "%s/objects", git_common_dir);
 	} else
 		git_db_env = 1;
 	git_index_file = getenv(INDEX_ENVIRONMENT);
@@ -171,6 +177,11 @@ const char *get_git_dir(void)
 	if (!git_dir)
 		setup_git_env();
 	return git_dir;
+}
+
+const char *get_git_common_dir(void)
+{
+	return git_common_dir;
 }
 
 const char *get_git_namespace(void)
